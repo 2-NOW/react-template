39b248ad1e174c037f8c51af979ceac54c1c63df
2-NOW <kj109888@gmail.com>
2023-07-12 00:32:52 +0900

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:react-hooks/recommended",
+    "plugin:tailwindcss/recommended",
+    "plugin:react/recommended",
+    "plugin:import/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["react-refresh", "react", "import"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "import/newline-after-import": "warn",
+    "tailwindcss/no-custom-classname": "off",
+    "@typescript-eslint/no-unused-vars": ["off", { argsIgnorePattern: "^_$" }],
+    "import/no-unresolved": ["off", { ignore: ["^@/"] }],
+    "import/order": [
+      "warn",
+      {
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+};
