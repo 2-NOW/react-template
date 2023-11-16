1be1fd862e5fa816d89d5f3f434bcd78bf39a05b
2-NOW <kj109888@gmail.com>
2023-11-17 01:44:23 +0900

    chore: Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-/// <reference types="vitest" />
-
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), tsconfigPaths()],
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
