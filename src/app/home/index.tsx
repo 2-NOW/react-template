4508129f802b4c607db9e5a3de20a21cc1d979bc
2-NOW <kj109888@gmail.com>
2024-06-16 16:03:09 +0900

    initial commit

--- src/app/home/index.tsx
+++
@@ -1,7 +0,0 @@
-export const Home = () => {
-  return (
-    <div>
-      <h1 className="bg-red-50 p-2 text-red-50">Home</h1>
-    </div>
-  );
-};
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from "vite";
+import path from "node:path";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	plugins: [react(), svgr()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
